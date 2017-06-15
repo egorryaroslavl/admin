d77225087ea3313251f4b538ed1e928d23b2b7e9
egorr <yaroslavl.city@gmail.com>
2017-06-15 21:59:10 +0300

    Изменена работа с иконками

--- src/AdminController.php
+++ src/AdminController.php
@@ -17,7 +17,8 @@
 
 		}
 
-		public function translite( Request $request ){
+		public function translite( Request $request )
+		{
 
 			$dictionary = array(
 				"А" => "a",
@@ -103,7 +104,7 @@
 				"<" => "",
 				">" => ""
 			);
-			$string     = preg_replace( '/[^\w\s]/u', ' ', $string );
+			$string     = preg_replace( '/[^\w\s]/u', ' ', $request->alias_source );
 			$string     = mb_strtolower( strtr( strip_tags( trim( $string ) ), $dictionary ) );
 			return preg_replace( '/[_]+/', '_', $string );
 		}
@@ -130,21 +131,138 @@
 			return $menu;
 		}
 
-	public static function iconsave( Request $request )
+
+		public static function iconsave( Request $request )
 		{
 			if( $request->hasFile( 'photo' ) ){
-				
-				$uploads_dir = sys_get_temp_dir();
-				$file     = $request->file( 'photo' );
-				$ext      = $file->clientExtension();
-				$tmp_name = $_FILES[ "photo" ][ "tmp_name" ];
-				/*$name     = basename( $_FILES[ "photo" ][ "name" ] );*/
-				$name = 'icon_' . $_POST[ 'table' ] . "_" . $_POST[ 'id' ] . ".$ext";
-				$res = move_uploaded_file( $tmp_name, "$uploads_dir/$name" );
-				if( $res > 0 ){
-					echo json_encode( [ 'error' => 'ok', 'message' => $uploads_dir . '/' . $name ] );
+
+				/* Помещаем файл во временную директорию.
+				При сохранении остальных данных заберём его оттуда */
+				$uploads_dir = sys_get_temp_dir(); // системный /tmp
+				$file        = $request->file( 'photo' );
+				$ext         = $file->clientExtension();
+
+
+				$name = 'icon_' . $request->table . "_" . $request->_token . ".png"; // новое имя файла
+
+
+				$img = \Image::make( $file )
+					->widen( config( 'admin.categories.icon_max_width' ), function ( $constraint ){
+						$constraint->upsize();
+					} )
+					->heighten( config( 'admin.categories.icon_max_height' ), function ( $constraint ){
+						$constraint->upsize();
+					} )->save( $uploads_dir . '/' . $name );
+
+
+				if( $img ){
+					$data = (string)$img->encode( 'data-url' );
+					return [ 'success'      => true,
+					         'error'        => 'ok',
+						//  'thumbnailUrl' => $img,
+						     'thumbnailUrl' => $data,
+						     'qquuid'       => $request->qquuid,
+						     'message'      => $uploads_dir . '/' . $name ];
 				}
 			}
+		}
+
+
+		public static function iconget( Request $request )
+		{
+
+
+			if( $request->id != 0 ){
+				$icon = \DB::table( $request->table )
+					->select( [ 'icon' ] )
+					->where( 'id', $request->id )->get();
+
+				$resArr = [
+					'success'      => false,
+					'name'         => '',
+					'thumbnailUrl' => '',
+					'uuid'         => $request->_token
+				];
+
+
+				if(
+					!is_null( $icon[ 0 ]->icon )
+					&& !empty( $icon[ 0 ]->icon )
+					&& file_exists( public_path( $icon[ 0 ]->icon ) )
+				)
+
+					$resArr = [
+						'success'      => true,
+						'name'         => $icon[ 0 ]->icon,
+						'thumbnailUrl' => $icon[ 0 ]->icon,
+						'uuid'         => $request->_token
+					];
+
+				return [ $resArr ];
+			}
+
+		}
+
+		public static function icondelete( Request $request )
+		{
+			if( $request->id != 0 ){
+				$icon = \DB::table( $request->table )
+					->select( [ 'icon' ] )
+					->where( 'id', $request->id )->get();
+
+
+				if( is_null( $icon[ 0 ]->icon ) or empty( $icon[ 0 ]->icon ) ){
+
+					return json_encode( [ 'error' => 'error', 'message' => 'В процессе удаления возникли ошибки!' ] );
+
+				}
+				$iconNameSmall = $icon[ 0 ]->icon;
+				$iconName      = str_replace( '_small', '', $iconNameSmall );
+
+				$iconNameSmallPath = public_path() . $iconNameSmall;
+				$iconNamePath      = public_path() . $iconName;
+
+				$iconNameSmallDel = false;
+				$iconNameDel      = false;
+				$resArr           = [
+					'error'        => 'error',
+					'success'      => false,
+					'name'         => null,
+					'thumbnailUrl' => null,
+					'qquuid'       => $request->qquuid,
+					'uuid'         => $request->qquuid
+				];
+
+
+				if( file_exists( $iconNameSmallPath ) ){
+					$iconNameSmallDel = unlink( $iconNameSmallPath );
+				}
+				if( file_exists( $iconNamePath ) ){
+					$iconNameDel = unlink( $iconNamePath );
+				}
+
+				if( $iconNameDel && $iconNameSmallDel ){
+
+					\DB::table( $request->table )
+						->where( 'id', $request->id )
+						->update( [ 'icon' => null ] );
+
+					$resArr = [
+						'error'        => 'ok',
+						'success'      => true,
+						'name'         => null,
+						'thumbnailUrl' => null,
+						'qquuid'       => $request->qquuid,
+						'uuid'         => $request->qquuid
+					];
+
+				}
+
+				return [ $resArr ];
+
+
+			}
+
 		}
 
 	}
--- src/views/common/icon.blade.php
+++ src/views/common/icon.blade.php
@@ -1,6 +1,152 @@
-<div class="dropzone-preview" id="icon-preview" data-table="{{$data->table}}" data-id="{{$data->id}}" data-icon="{{$data->icon}}"></div><div id="icon-place" class="dropzone"></div>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.0.1/min/dropzone.min.css" rel="stylesheet">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
-<script>var paramsData={table:"{{$data->table}}",id:'{{$data->id,"0"}}',_token:"<?php echo csrf_token() ?>"};Dropzone.autoDiscover=!1;var imgDropzone=new Dropzone("div#icon-place",{url:"/iconsave",params:paramsData,uploadMultiple:!1,addRemoveLinks:!0,maxFiles:1,thumbnailWidth:200,thumbnailHeight:200,paramName:"photo",dictDefaultMessage:"Бросьте изображение сюда.<br>Или кликните здесь.",dictRemoveFile:"Удалить",init:function(){this.on("success",function(e,o){var a=jQuery.parseJSON(o);"ok"==a.error&&($("div.dz-image").each(function(e,o){o.index>0&&$(o).remove()}),$("input[name='icon']").remove(),$("form").append('<input type="hidden" name="icon" id="icon" value="'+a.message+'" />'),$(".dropzone-preview").css("opacity",".1")),"error"==a.error&&alert("Загрузка изображения закончилось ошибкой!")}),this.on("removedfile",function(){imgDropzone.enable()})}});imgDropzone.on("complete",function(){imgDropzone.disable()});
+<script src="/_admin/js/plugins/fine-uploader/fine-uploader.js"></script>
+<script type="text/template" id="qq-template-icon">
+	<div class="qq-uploader-selector">
+		<div class="row">
+			<div class="col-md-12">
+ 				<div class="btn btn-info btn-block qq-upload-button-selector qq-upload-button"><i class="fa fa-image"></i> Выбрать файл
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="thumbnail">
+					<div class="caption"
+					     style="min-height: 220px;background: url('/_admin/img/drop-here.png') 50% 50% no-repeat;background-size: contain;padding:5px 0">
+						<div class=" qq-upload-drop-area-selector" style="width: 100%;height:150px"
+						     qq-hide-dropzone></div>
+						<ul class="qq-upload-list-selector list-unstyled" role="region" aria-live="polite"
+						    aria-relevant="additions removals" style="padding: 0;margin: 0">
+							<li><span role="status" class="qq-upload-status-text-selector qq-upload-status-text"></span>
+								<div class="qq-progress-bar-container-selector qq-progress-bar-container">
+									<div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"
+									     class="qq-progress-bar-selector qq-progress-bar"></div>
+								</div>
+								<span class="qq-upload-spinner-selector qq-upload-spinner"></span>
+								<div class="qq-thumbnail-wrapper">
+									<img src="{{  $data->icon   or ''}}" id="picture"
+									     class="qq-thumbnail-selector img-responsive img-thumbnail" qq-server-scale>
+								</div>
+ 
+								<button type="button"
+								        class="btn btn-block btn-danger qq-upload-delete-selector qq-upload-delete">
+									<i class="fa fa-trash"></i> Удалить
+								</button>
+								
+								<button type="button" class="btn btn-block btn-warning qq-upload-cancel-selector">
+									<i class="fa fa-ban"></i> Закрыть
+								</button>
+							
+							</li>
+						</ul>
+				
+						<dialog class="qq-alert-dialog-selector">
+							<div class="qq-dialog-message-selector"></div>
+							<button type="button" id="delete-icon" class="btn btn-info qq-cancel-button-selector">
+								Закрыть
+							</button>
+						</dialog>
+					</div>
+				</div>
+			</div>
+		</div>
+ 	</div>
 </script>
-<style>.dropzone{padding:3px;text-align:center;vertical-align:middle}.dropzone-preview{border:1px #6a6a6a dotted!important;border-bottom:none;width:224px;height:150px;background:transparent url({{$data->icon}}) 50% 50% no-repeat;background-size:contain}.dz-image{border-radius:2px!important;overflow:hidden;width:200px!important;height:200px!important;position:relative;display:block;z-index:10}.data-dz-thumbnail{border-radius:3px!important;height:120px;width:120px}.dz-remove{background:#ff9a00;color:#fff;padding:2px 10px;border-radius:6px;margin-top:10px}.dropzone .dz-preview{margin:0;padding:0}.dz-remove:hover{background:#ff6000;color:#fff;text-decoration:none!important}</style>
+<div id="fine-uploader-icon"></div>
+<script>
+ $( function(){
+
+		var uploader = new qq.FineUploader( {
+			element   : document.getElementById( "fine-uploader-icon" ),
+			template  : 'qq-template-icon',
+			debug     : true,
+			multiple  : false,
+			request   : {
+				endpoint : '/iconsave',
+				inputName: 'photo',
+				params   : {
+					table : "{{$data->table}}",
+					id    : '{{$data->id,"0"}}',
+					_token: "{{csrf_token()}}"
+				}
+			},
+			deleteFile: {
+				enabled     : true,
+				forceConfirm: true,
+				method      : "POST",
+				endpoint    : '/icondelete',
+				params      : {
+					table : "{{$data->table}}",
+					id    : '{{$data->id,"0"}}',
+					_token: "{{csrf_token()}}"
+				}
+			},
+			validation: {
+				allowedExtensions: [ 'jpeg', 'jpg', 'gif', 'png' ],
+				sizeLimit        : 2000000,
+				image            : {
+					minHeight: 50,
+					minWidth : 50
+				}
+
+			},
+			session   : {
+				endpoint: '/iconget',
+				params  : {
+					table : "{{$data->table}}",
+					id    : '{{$data->id,"0"}}',
+					_token: "{{csrf_token()}}",
+
+				}
+			},
+
+			thumbnails: {
+				placeholders: {
+					waitingPath: '/_admin/js/plugins/fine-uploader/placeholders/waiting-generic.png',
+ 
+				}
+			},
+
+			callbacks: {
+				onSessionRequestComplete: function( response, success, xhrOrXdr ){
+
+					if( !response.success ){
+
+					/*	$( ".qq-upload-delete-selector" ).hide();*/
+						$( ".qq-upload-cancel-selector" ).hide();
+
+					}
+
+				},
+				onComplete              : function( id, fileName, responseJSON, xhr ){
+
+					if( responseJSON.success ){
+
+						$( "#icon" ).remove();
+						$( '#fine-uploader-icon' ).append( '<input type="hidden" name="icon" id="icon" value="' + responseJSON.message + '" />' );
+					}
+
+
+				},
+				onCancel                : function(){
+					$( '.caption' ).animate( {
+						'background-position-y': 0
+					}, 900, function(){
+					} );
+				}
+
+			},
+
+			messages: {
+				sizeError          : 'Файл слишком велик!  Лимит - {sizeLimit}.',
+				typeError          : 'Файл имеет недопустимый тип. Допустимые типы: {extensions}.',
+				minHeightImageError: 'Высота изображения недопустимо мала!',
+				minWidthImageError : 'Ширина изображения недопустимо мала!',
+				onLeave            : 'Загрузка файла ещё не завершена! Если вы покините страницу процесс будет прерван!'
+			}
+
+
+		} );
+ 
+	} );
+</script>
+<style>.qq-alert-dialog-selector{-webkit-box-shadow:2px 6px 15px 0 rgba(50,50,50,0.75);-moz-box-shadow:2px 6px 15px 0 rgba(50,50,50,0.75);box-shadow:2px 6px 15px 0 rgba(50,50,50,0.75);border:1px #9999c1 solid;border-radius:6px;background:-moz-linear-gradient(90deg,#fff 80%,#ff1e00 80%);background:-webkit-linear-gradient(90deg,#fff 80%,#ff1e00 80%);background:-o-linear-gradient(90deg,#fff 80%,#ff1e00 80%);background:linear-gradient(0deg,#fff 80%,#ff1e00 80%)}.qq-dialog-message-selector{padding:20px;font-size:20px;margin-top:10px}.qq-upload-list-selector{padding:0;margin:0}.qq-upload-list-selector li{padding:0;margin:0;text-align:center}.qq-gallery.qq-uploader{position:relative;min-height:220px;overflow-y:hidden;width:inherit;border-radius:6px;border:1px dashed #CCC;background-color:#FAFAFA;padding:8px}</style>
